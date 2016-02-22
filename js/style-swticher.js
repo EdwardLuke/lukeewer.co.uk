720586d631955d8136339a102e1004757e905d19
EdwardLuke7 <ewer.wtsonline@gmail.com>
2016-02-22 23:12:44 +0000

    Initial v1 site commit

---
+++ js/style-swticher.js
@@ -0,0 +1,8 @@
+/* Setup */
+var d = window.document;
+var button = d.getElementsByClassName("css-controls").getElementsByTagName("button");
+button.addEventListener("click", switchCSS);
+
+/* Functions */
+var switchStyle = function() {
+
